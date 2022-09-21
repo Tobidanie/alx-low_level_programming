0c0a45231190834bd6b2cdede45956613f9dd0dd
Tobidanie <oladipupodaniel6@gmail.com>
2022-09-20 18:58:53 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * puts - prints a string and a new line
+ * _puts - prints a string and a new line
  * @str: pointer to a string to print
  *
  * Return: 0
  */
 
-void puts(char *str)
+void _puts(char *str)
 {
 	int i = 0;
 
