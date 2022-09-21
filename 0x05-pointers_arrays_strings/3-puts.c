bf0b720746d7b616a0c6d9ebe52a122f35335ed4
Tobidanie <oladipupodaniel6@gmail.com>
2022-09-20 18:53:55 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * puts - prints a string and a new line
+ * @str: pointer to a string to print
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
