92aa48c84b7ee00278fed6f97ff5569e00088027
Tobidanie <oladipupodaniel6@gmail.com>
2022-09-20 19:46:04 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * puts_half - prints the second of the string
+ * @str: the string reference
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
