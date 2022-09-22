b0d9c667699ad259f95b6df88530f883a7560fd2
Tobidanie <oladipupodaniel6@gmail.com>
2022-09-22 11:31:16 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,9 @@
  *
  * Return: If str1 < str1, the negative difference of
  * the first ummatched chararcters.
- * If s1 == s2, 0.
- * If s1 > s2, the positive differrence of 
- * the unmatched string unmatched characters.
+ * 	   If s1 == s2, 0.
+ *	   If s1 > s2, the positive differrence of 
+ *	   the unmatched string unmatched characters.
  */
 int _strcmp(char *s1, char *s2)
 {
