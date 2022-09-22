ea49d2b5b1cc22b8f80e2aaa6456e9c4bd73db16
Tobidanie <oladipupodaniel6@gmail.com>
2022-09-22 12:12:41 -0700

    rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *
+ * reverse_array - reverse aaray of integers
+ * @a: array
+ * @n: number of elements of array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t
+	}
+	return (0);
+}
